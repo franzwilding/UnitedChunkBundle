d3f760be4b416054649e888210bf992efc0cf900
Franz Wilding <franz@2robots.at>
2015-06-01 10:02:42 +0200

    hide select on chunk select

--- Resources/public/js/UnitedChunk.js
+++ Resources/public/js/UnitedChunk.js
@@ -25,16 +25,17 @@ UnitedOne.modules.UnitedChunk = {
             var chunks  = [];
             var current = null;
 
+            var select = $('div[data-chunk-select="data-chunk-select"]', context);
             $('div[data-chunk-chunk="data-chunk-chunk"]', context).each(function(){
                 chunks[$(this).data('chunk-id')] = $(this);
                 t.hideChunk($(this));
                 if($(this).find('input[name$="[id]"]').val()) {
                     t.showChunk($(this));
                     current = $(this);
+                    select.hide();
                 }
             });
 
-            var select = $('div[data-chunk-select="data-chunk-select"]', context);
             $('input[type="radio"]', select).change(function(){
                 if(current) {
                     t.hideChunk(current);
@@ -42,6 +43,7 @@ UnitedOne.modules.UnitedChunk = {
 
                 current = chunks[$(this).val()];
                 t.showChunk(current);
+                select.hide();
             });
         });
     }
